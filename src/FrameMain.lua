2d0b96bd8e7624c2ad9ac477036771c1cfca01aa
Sharm <for.sneg@gmail.com>
2013-05-12 02:00:17 +0400

    Added second tab button

--- src/FrameMain.lua
+++ src/FrameMain.lua
@@ -1,57 +1,8 @@
 -- Author: for.sneg@gmail.com
 
-function btnStart_OnClick()
-	if frameMain:IsShown() then
-		frameMain:Hide();
-	else
-		frameMain:Show();
-	end
-end
-
-
-function btnDump_OnClick()
-
-	Log:msg("123123123 " .. Log.scrollFrame:GetVerticalScrollRange() .. " - " .. Log.scrollFrame:GetVerticalScroll())
-	
-	
-	-- private:CreateCharDump();
-	-- private:SaveCharData(private.Encode(private.GetCharDump()))
-end
-
-function btnClose_OnClick()
-	frameMain:Hide();
-end
-
-function logScroll_OnVerticalScroll()
-	
-end
-
-function testInit()
-	info = {
-		text = '12312313123';
-		func = nil;
-	};
-	UIDropDownMenu_AddButton(info);
-	UIDropDownMenu_AddButton(info);
-	UIDropDownMenu_AddButton(info);
-	UIDropDownMenu_AddButton(info);
-end
-
-function frameMain_OnLoad()
-	frameMain:RegisterForDrag("LeftButton")
-	Log:init(frameMain)
-
-	UIDropDownMenu_Initialize(test, testInit);
-end
-
-function tabDump_OnClick()
-	tabDump:setActive(not tabDump:isActive())
-end
-
-function frameMain_OnShow()
-	tabDump:setActive(false);
-end
-
+--
+-- Components initialize and definitions
+--
 function HighlightTab_Initialize(self)
 
 	function self:isActive()
@@ -72,5 +23,70 @@ function HighlightTab_Initialize(self)
 			self:SetNormalTexture(nil)
 		end
 	end
+	
+	-- Constructor
+	_G[self:GetName().."RightDisabled"]:Hide()
+	_G[self:GetName().."LeftDisabled"]:Hide()
+	_G[self:GetName().."MiddleDisabled"]:Hide()
+	_G[self:GetName().."Right"]:Show()
+	_G[self:GetName().."Left"]:Show()
+	_G[self:GetName().."Middle"]:Show()
+	
+end
 
+
+--
+-- Main frame loaded
+--
+function frameMain_OnLoad()
+	frameMain:RegisterForDrag("LeftButton")
+
+	-- Create log scrollframe
+	Log:init(frameMain)
+
+	UIDropDownMenu_Initialize(test, testInit)
+end
+
+
+--
+-- Load and prepare components
+--
+function frameMain_OnShow()
+	tabDump:setActive(true)
+	tabRestore:setActive(false)
+end
+
+
+--
+-- Events on frame
+--
+function tab_OnClick(self, otherTab)
+	self:setActive(true)
+	otherTab:setActive(not self:isActive())
+end
+
+
+function btnDump_OnClick()
+
+	Log:msg("123123123 " .. Log.scrollFrame:GetVerticalScrollRange() .. " - " .. Log.scrollFrame:GetVerticalScroll())
+	
+	
+	-- private:CreateCharDump();
+	-- private:SaveCharData(private.Encode(private.GetCharDump()))
+end
+
+function btnClose_OnClick()
+	frameMain:Hide();
+end
+
+
+function testInit()
+	info = {
+		text = '12312313123';
+		func = nil;
+	};
+	UIDropDownMenu_AddButton(info);
+	UIDropDownMenu_AddButton(info);
+	UIDropDownMenu_AddButton(info);
+	UIDropDownMenu_AddButton(info);
 end
