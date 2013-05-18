3db587c6204c32569016fb93ff33408a279d25e2
Sharm <for.sneg@gmail.com>
2013-05-18 04:29:12 +0400

    Change include order

---
+++ src/addon.lua
@@ -0,0 +1,6 @@
+﻿-- Author: for.sneg@gmail.com
+
+VERSION = "v1.1"
+ADDONNAME = "CharDump-PRO "..VERSION
+
+Addon = LibStub("AceAddon-3.0"):NewAddon(ADDONNAME, "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0")
--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,4 @@
-﻿
-VERSION = "v1.1"
-ADDONNAME = "CharDump-PRO "..VERSION
-
-Addon = LibStub("AceAddon-3.0"):NewAddon(ADDONNAME, "AceConsole-3.0", "AceEvent-3.0")
+﻿-- Author: for.sneg@gmail.com
 
 Addon:RegisterChatCommand("cd", "slash")
 
